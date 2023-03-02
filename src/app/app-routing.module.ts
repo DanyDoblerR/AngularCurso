4bed103c7e34e6ddf1685eea91f62b3207c5c12b
OsmarRojas <enrique.rojas@cascadacon.com>
2023-03-02 13:37:11 -0600

    se agregaron las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const rutas: Routes = [
+  { path: '', redirectTo: '/inicio-sesion', pathMatch: 'full' },
+  {path: 'inicio-sesion', component: ''},
+  {path:'registrar-usuario', component: ''},
+  {path: 'restablecer-contrasena', component: ''},
+  {path: 'panel-control', component: '',
+    children: [
+      {path: '', redirectTo: 'mostrar-usuarios', pathMatch: 'full'},
+      {path: 'mostrar-usuarios', component: ''},
+      {path: 'informacion-usuario', component: ''},
+      {path: 'editar-usuario', component: ''},
+    ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
