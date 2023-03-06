0a035945c9cada4fdbf813495ac2ad5776eb2aa7
Darbroda1801 <dario.torres@cascadacon.com>
2023-03-06 00:08:25 -0600

    añadi funcionalidades

--- src/app/Dashboard/dashboard.component.ts
+++ src/app/Dashboard/dashboard.component.ts
@@ -3,21 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'app-name',
     template : `
-    <div class="container">
+    <div>
         <router-outlet></router-outlet>
     </div>
-    `
-    ,
-    styles: [`
-        .container {
-            height: 100vh;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            background-color: aliceblue;
-        }
-    `]
-
+    `,
 })
 export class DashboardComponent implements OnInit {
     constructor() { }
